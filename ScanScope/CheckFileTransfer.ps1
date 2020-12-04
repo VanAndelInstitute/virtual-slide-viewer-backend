4de05cd901238aa220e7ff5e41a509565ae353c0
rmontroy <rob.montroy@vai.org>
2020-12-04 12:57:25 -0500

    PowerShell script for ScanScope machine

---
+++ ScanScope/CheckFileTransfer.ps1
@@ -0,0 +1,14 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$uri = "https://path-uat.cptac.vai.org/CheckFileTransferStatus"
+cd D:\Images\CPTAC
+$list = ls *.svs | ForEach-Object {@{"filename" = $_.Name; "size" = $_.Length}}
+If($list.Count -eq 0)
+{
+    return
+}
+$body = @{"files" = $list} | ConvertTo-Json
+$result = Invoke-RestMethod -Method 'POST' -Uri $uri -Body $body
+ForEach($filename in $result)
+{
+    del $filename
+} 
