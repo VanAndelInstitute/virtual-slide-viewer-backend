11c8979ecc30925ccdbb01897380c73895a4c86d
rmontroy <rob.montroy@vai.org>
2020-12-28 10:57:34 -0500

    rename import slide script; add scan timing check

--- ScanScope/ImportSlide.ps1
+++ ScanScope/ImportSlide.ps1
@@ -1,7 +1,8 @@
 ﻿param($Hostname)
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 ls *.svs | ForEach-Object -Parallel {
+    If (((Get-Date) - $_.LastWriteTime).TotalSeconds -lt 30) {return}
     $body = ConvertTo-Json @{"filename" = $_.Name; "size" = $_.Length}
     $result = Invoke-RestMethod -Method 'POST' -Uri "https://${using:Hostname}/ImportSlide" -Body $body
-    del $result.filename
+    Remove-Item $result.filename
 } 
