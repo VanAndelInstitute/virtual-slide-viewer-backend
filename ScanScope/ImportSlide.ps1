5e9bb62ad4ef7abcbf423677fd98c311d4e98439
rmontroy <rob.montroy@vai.org>
2021-03-22 19:20:28 -0400

    fix svs import age check

--- ScanScope/ImportSlide.ps1
+++ ScanScope/ImportSlide.ps1
@@ -1,7 +1,9 @@
 ﻿param($Hostname)
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 ls *.svs | ForEach-Object -Parallel {
-    If (((Get-Date) - $_.LastWriteTime).TotalSeconds -lt 30) {return}
+    # Don't start before Scanner is done writing (>2 minutes old?), and give up after file is 22 minutes old
+    $age = ((Get-Date) - $_.LastWriteTime).TotalMinutes
+    If (($age -lt 2) -or ($age -gt 22)) {return}
     $body = ConvertTo-Json @{"filename" = $_.Name; "size" = $_.Length}
     $result = Invoke-RestMethod -Method 'POST' -Uri "https://${using:Hostname}/ImportSlide" -Body $body
     Remove-Item $result.filename
